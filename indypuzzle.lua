4d0eb6c15962839b44f2ccffd789647d50db3850
luischavesdev <115810776+luischavesdev@users.noreply.github.com>
2025-10-26 12:05:17 +0000

    linked indy game with final art

--- indypuzzle.lua
+++ indypuzzle.lua
@@ -1,13 +1,23 @@
 -- stores first sprite of each row - 1
-puzzle_sprites = { 27, 43, 59 }
+puzzle_sprites = { 74, 90, 106, 122 }
+indy_sprites = { 65, 66 }
+indy_anim_index = 1
+
+indy_location_x = 40
+indy_location_y = 80
 
 piece_draw_offset = 9
 piece_size = 8
+piece_grid_side = 4
 inbetween_piece_distance = 0
-x_puzzle_offset = 40
-y_puzzle_offset = 40
 
--- 0-3 grid coordinate that is currently selected
+cam_pos_puzzle_x = 160
+cam_pos_puzzle_y = 0
+
+x_puzzle_offset = 70
+y_puzzle_offset = 48
+
+-- 0-piece_grid_side grid coordinate that is currently selected
 x_selection = 0
 y_selection = 0
 
@@ -15,15 +25,15 @@ y_selection = 0
 -- Values: 0=NoFlipping, 1=HFlipping, 2=VFlipping, 3=BothFlipping
 puzzle_flip_state = {}
 
-bckgrnd_color = 1
+bckgrnd_color = 14
 selection_color = 8
 
 puzzle_completed = false
 
 
-function check_win_state()
-	for x = 1, 3 do
-		for y = 1, 3 do
+function check_puzzle_win_state()
+	for x = 1, piece_grid_side do
+		for y = 1, piece_grid_side do
 			if puzzle_flip_state[x][y] != 0 then
 				return false
 			end
@@ -80,60 +90,84 @@ function handle_flip_input(flip_horizontal)
 end
 
 
-function _init()
-	color(selection_color)
+function init_indy()
+	cam.x = cam_pos_puzzle_x
+	cam.y = cam_pos_puzzle_y
+
+	x_puzzle_offset = cam_pos_puzzle_x + x_puzzle_offset
+	y_puzzle_offset = cam_pos_puzzle_y + y_puzzle_offset
+
+	indy_location_x = cam_pos_puzzle_x + indy_location_x
+	indy_location_y = cam_pos_puzzle_y + indy_location_y
+
 	inbetween_piece_distance = piece_draw_offset - piece_size
 
 	-- setup puzzle_flip_state
-	for x = 1, 3 do
+	for x = 1, piece_grid_side do
 		puzzle_flip_state[x] = {}
-		for y = 1, 3 do
+		for y = 1, piece_grid_side do
 			-- a random integer between [0 and 3]
 			-- pray that it doesnt turn out to be all 0s
-			puzzle_flip_state[x][y] = flr(rnd(4))
+			--puzzle_flip_state[x][y] = flr(rnd(4))
+			puzzle_flip_state[x][y] = 0
 		end
 	end
 end
 
-function _update()
+function update_indy()
 	if btnp(⬅️) then
-		x_selection = mid(x_selection - 1, 2)
+		x_selection = mid(x_selection - 1, piece_grid_side - 1)
 	end
 
 	if btnp(➡️) then
-		x_selection = mid(x_selection + 1, 2)
+		x_selection = mid(x_selection + 1, piece_grid_side - 1)
 	end
 
 	if btnp(⬆️) then
-		y_selection = mid(y_selection - 1, 2)
+		y_selection = mid(y_selection - 1, piece_grid_side - 1)
 	end
 
 	if btnp(⬇️) then
-		y_selection = mid(y_selection + 1, 2)
+		y_selection = mid(y_selection + 1, piece_grid_side - 1)
 	end
 
 	if btnp(🅾️) then
 		if puzzle_completed == false then
 			handle_flip_input(true)
-			puzzle_completed = check_win_state()
+			puzzle_completed = check_puzzle_win_state()
 		end
 	end
 
 	if btnp(❎) then
 		if puzzle_completed == false then
 			handle_flip_input(false)
-			puzzle_completed = check_win_state()
+			puzzle_completed = check_puzzle_win_state()
 		end
 	end
 end
 
-function _draw()
+function draw_indy()
 	cls(bckgrnd_color)
+	camera(cam.x, cam.y)
+
+	-- door
+	if puzzle_completed == false then
+		rectfill(cam.x + 30, cam.y + 65, cam.x + 40, cam.x + 80, 0)	
+	end
+
+	-- map
+	map()
+
+	-- draw puzzle background
+	local background_side = (piece_grid_side * piece_size) + ((piece_grid_side - 2) * inbetween_piece_distance)
+	local puzzle_background_extent_x = x_puzzle_offset + background_side
+	local puzzle_background_extent_y = y_puzzle_offset + background_side
+	rectfill(x_puzzle_offset, y_puzzle_offset, puzzle_background_extent_x, puzzle_background_extent_y, 0)
 
 	-- draw puzzle pieces
-	for ypiece = 1, 3 do
-		for xpiece = 1, 3 do
-			sprite_index = puzzle_sprites[ypiece] + xpiece
+	for ypiece = 1, piece_grid_side do
+		for xpiece = 1, piece_grid_side do
+			local sprite_index = puzzle_sprites[ypiece] + xpiece
 			local x_pos = x_puzzle_offset + (xpiece - 1) * piece_draw_offset
 			local y_pos = y_puzzle_offset + (ypiece - 1) * piece_draw_offset
 
@@ -144,12 +178,27 @@ function _draw()
 		end
 	end
 
+	-- draw selection rectangle
 	local selection_coord_x = x_puzzle_offset + x_selection * (piece_size + inbetween_piece_distance) - 1
 	local selection_coord_y = y_puzzle_offset + y_selection * (piece_size + inbetween_piece_distance) - 1
 
 	local x_selection_size = selection_coord_x + piece_size + 1
 	local selection_size_coord_y = selection_coord_y + piece_size + 1
 
-	-- draw selection rectangle
-	rect(selection_coord_x, selection_coord_y, x_selection_size, selection_size_coord_y)
+	rect(selection_coord_x, selection_coord_y, x_selection_size, selection_size_coord_y, selection_color)
+
+	-- draw indy
+	if  puzzle_completed then
+		if indy_anim_index > 2.9 then
+			indy_anim_index = 1
+		else
+			indy_anim_index = indy_anim_index + 0.1
+		end
+	end
+	spr(indy_sprites[flr(indy_anim_index)], indy_location_x, indy_location_y)
+
+
+	print("complete picture.", cam.x + 30, cam.y + 20, 0)
+	print("flip tiles with 🅾️ and ❎", cam.x + 15, cam.y + 30, 0)
+
 end
